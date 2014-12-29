a16ced0db4ed53b397dee1c92cfd6b57f8a6f8e8
Ernesto Laval <ernesto.laval@gmail.com>
2014-12-29 19:50:36 +0000

    Remove tide-angular-xy from tide-angular main file

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -41,39 +41,12 @@ NG_DOCS={
     },
     {
       "section": "api",
-      "id": "tide-angular.directive:tdXyChart",
-      "shortName": "tdXyChart",
-      "type": "directive",
-      "moduleName": "tide-angular",
-      "shortDescription": "Generates a scatered XY Chart",
-      "keywords": "api array attribute axis bubbles categories category chart codes color configuration d3 d3service data define defining directive display displayed div element entities function generates graph height indicates left legend linearregression margin maximum minimun object objects options point populating regression return returns scale scatered selected set shoudl size sqr tdcolorattribute tdcolorlegend tddata tdidattribute tdmaxsize tdminsize tdoptions tdselected tdsizeattribute tdsqrscalex tdtooltipmessage tdtrendline tdwidth tdxattribute tdyattribute teh text tide-angular tidelayoutxy tooltip trendline underscore unique values wether widht xy"
-    },
-    {
-      "section": "api",
-      "id": "tide-angular.linearRegression",
-      "shortName": "tide-angular.linearRegression",
-      "type": "service",
-      "moduleName": "tide-angular",
-      "shortDescription": "Calculates linear regression parameters for a set of datapoints [[x1,y1], [x2,y2], ... [xn,yn]]",
-      "keywords": "api calculates datapoints intercept linear linearregression object parameters points r2 regression returns service set slope tide-angular underscore y1 y2 yn"
-    },
-    {
-      "section": "api",
       "id": "tide-angular.tideLayoutTreemap",
       "shortName": "tide-angular.tideLayoutTreemap",
       "type": "service",
       "moduleName": "tide-angular",
-      "shortDescription": "Creates a layout (node array with position attributes) for building a TreeMap",
-      "keywords": "api argument array attribute attributes attrribute based building category color colors column columns component creates current d3 d3service data defines displayin dx dy function generates grouping height including info inpit instance labels layout nodes number objects params position respective service sets size students tide-angular tidelayouttreemap title titles treemap type underscore units width zone"
-    },
-    {
-      "section": "api",
-      "id": "tide-angular.tideLayoutXY",
-      "shortName": "tide-angular.tideLayoutXY",
-      "type": "service",
-      "moduleName": "tide-angular",
-      "shortDescription": "Creates a layout (node array with position attributes) for building an XY bubble chart",
-      "keywords": "api array attributes bubble building chart circle coordinates creates d3 d3service data displayed function generates layout nodes objects position radious service tide-angular tidelayoutxy underscore xy"
+      "shortDescription": "Creates a layout (node array with position attributes) for building an Treemap Chart",
+      "keywords": "api array attributes building chart creates d3 d3service layout position service tide-angular tidelayouttreemap treemap underscore"
     },
     {
       "section": "api",
